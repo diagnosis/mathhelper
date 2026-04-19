b463f5d7c8bb0f42cdd6937d229855ed4cffb7ec
diagnosis <demirkansafa@gmail.com>
2026-04-19 20:41:49 +0300

    add godoc comments

--- mathhelper.go
+++ mathhelper.go
@@ -1,5 +1,10 @@
+// Package mathhelper provides simple mathematical helper functions.
 package mathhelper
 
+// Add returns the sum of a and b.
+//
+// For more on addition, see
+// https://www.mathsisfun.com/numbers/addition.html.
 func Add(a, b int) int {
 	return a + b
 }
